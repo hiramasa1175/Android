dba9c970b430b3c344acefebebb7844fe8a96f83
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-21 22:26:45 +0900

    fix

--- NumberPicker/app/src/main/java/jp/hiramasa/numberpicker/MainActivity.kt
+++ NumberPicker/app/src/main/java/jp/hiramasa/numberpicker/MainActivity.kt
@@ -1,28 +1,47 @@
 package jp.hiramasa.numberpicker
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.LayoutInflater
+import android.support.v7.app.AlertDialog
+import kotlinx.android.synthetic.main.off_dialog.view.*
 
 class MainActivity : AppCompatActivity() {
 
-  var formula = 1000
-  var result = ""
+  var off = ""
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    numPicker1.maxValue = 9
-    numPicker1.minValue = 0
-
-    numPicker2.maxValue = 9
-    numPicker2.minValue = 0
-
     button1.setOnClickListener {
-      result = numPicker1.value.toString() + numPicker2.value.toString()
-
-      println(result.toInt())
+      setOff()
     }
+  }
+
+  private fun setOff() {
+    val inflater = this.getSystemService(
+        Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    val layout = inflater.inflate(R.layout.off_dialog,
+        findViewById(R.id.layout_root))
+
+    layout.numPicker1.minValue = 0
+    layout.numPicker1.maxValue = 9
+
+    layout.numPicker2.minValue = 0
+    layout.numPicker2.maxValue = 9
+
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle("～パーセント引き")
+    builder.setView(layout)
+    builder.setPositiveButton("OK") { _, _ ->
+      off = layout.numPicker1.value.toString() + layout.numPicker2.value.toString()
+    }
+    builder.setNegativeButton("Cancel") { _, _ ->
+    }
+
+    builder.create().show()
   }
 }
