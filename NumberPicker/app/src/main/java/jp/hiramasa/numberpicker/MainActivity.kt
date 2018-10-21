1e072e90d4f9d9590194bbeeb9767fec83b0a076
hiramasa1175 <hiramasa1175@gmail.com>
2018-10-21 19:22:02 +0900

    test

--- NumberPicker/app/src/main/java/jp/hiramasa/numberpicker/MainActivity.kt
+++ NumberPicker/app/src/main/java/jp/hiramasa/numberpicker/MainActivity.kt
@@ -2,11 +2,27 @@ package jp.hiramasa.numberpicker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  var formula = 1000
+  var result = ""
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    numPicker1.maxValue = 9
+    numPicker1.minValue = 0
+
+    numPicker2.maxValue = 9
+    numPicker2.minValue = 0
+
+    button1.setOnClickListener {
+      result = numPicker1.value.toString() + numPicker2.value.toString()
+
+      println(result.toInt())
     }
+  }
 }
